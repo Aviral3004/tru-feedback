e1e54178f333cea2065d9e3746c8e30c34bf6712
Aviral Kaushal <guest3@Avirals-MacBook-Pro.local>
2025-09-12 11:44:38 +0530

    authOptions using nextAuth implemented

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,79 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOptions: NextAuthOptions = {
+  //* You put here differenct auth providers like google, credentials(for you custom email, password auth provider), github etc.
+  //* for each auth provider give id, name, credentials, authorize (for credentials (your custom provider))
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        identifier: { label: "Email or Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { username: credentials.identifier },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+
+          if (user.isVerified) {
+            throw new Error("Please verify your account first before login");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect Password!");
+          }
+        } catch (err: any) {}
+      },
+    }),
+  ],
+  //* For adding you own custom login, signup pages you can call these routes
+  pages: {
+    signIn: "/sign-in",
+  },
+  //* callbacks
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
