8bc23ba0c640e424f524e819978d47af936139a1
Aviral Kaushal <guest3@Avirals-MacBook-Pro.local>
2025-09-15 16:32:03 +0530

    implemented get-messages, send-message, accept-message api routes

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,114 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+
+//* POST api for accepting message when logged in user clicks on the toggle
+export async function POST(request: Request) {
+  await dbConnect();
+
+  //* We get the session of the logged in user by providing authOptions into getServerSession from next-auth
+  const session = await getServerSession(authOptions);
+  //* This User imported form next-auth helps in giving the type of the user which is specified below
+  //* User :- defines the shape of session.user for type safety
+  const user: User = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      userId,
+      { isAcceptingMessage: acceptMessages },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update user status to accept messages",
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        message: "Message acceptance status updated succesfully",
+        updatedUser,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Failed to update user status to accept messages");
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to update user status to accept messages",
+      },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  await dbConnect();
+
+  //* We get the session of the logged in user by providing authOptions into getServerSession from next-auth
+  const session = await getServerSession(authOptions);
+  //* This User imported form next-auth helps in giving the type of the user which is specified below
+  //* User :- defines the shape of session.user for type safety
+  const user: User = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = user._id;
+  try {
+    const foundUser = await UserModel.findById(userId);
+    if (!foundUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        isAcceptingMessages: foundUser.isAcceptingMessage,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("Error in getting message acceptance status");
+    return Response.json(
+      {
+        success: false,
+        message: "Error in getting message acceptance status",
+      },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,69 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET() {
+  await dbConnect();
+
+  //* We get the session of the logged in user by providing authOptions into getServerSession from next-auth
+  const session = await getServerSession(authOptions);
+  //* This User imported form next-auth helps in giving the type of the user which is specified below
+  //* User :- defines the shape of session.user for type safety
+  const user: User = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      { status: 401 }
+    );
+  }
+
+  const userId = new mongoose.Types.ObjectId(user._id); //* This conversion is needed for aggregation pipeline so proper mongoose id is needed not in string
+  try {
+    const user = await UserModel.aggregate([
+      //* pipelines
+      { $match: { id: userId } },
+
+      //* now we add pipeline for unwinding arrays (converting each array element to corresponding each object)
+      { $unwind: "$messages" },
+
+      //* adding pipeline to sort in ascending order by giving -1
+      { $sort: { "messages.createdAt": -1 } },
+
+      //* Now we finally add pipeline to group all these objects
+      { $group: { _id: "$_id", messages: { $push: "$messages" } } },
+    ]);
+
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "User not found",
+        },
+        { status: 401 }
+      );
+    }
+
+    return Response.json(
+      {
+        success: true,
+        messages: user[0].messages,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("An unexpected error occured:", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Internal server error",
+      },
+      { status: 500 }
+    );
+  }
+}
