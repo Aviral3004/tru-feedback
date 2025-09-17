160d924a4f75e236e40fef8a86f028ffa34c4bf8
Aviral Kaushal <guest3@Avirals-MacBook-Pro.local>
2025-09-17 13:00:29 +0530

    implemented sign-up page and verification of user

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,7 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import React from "react";
 
-export default function Component() {
-  const { data: session } = useSession();
-  //* Here AuthProvider was needed for session as we need to use SessionProvider which is inside /context/AuthProvider.tsx (this is vvimp!)
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
-  return (
-    <>
-      Not signed in <br />
-      <button
-        className="bg-orange-500 p-3 px-3 py-1 m-4 rounded"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
-}
+const SignIn = () => {
+  return <div>SignIn Page</div>;
+};
+
+export default SignIn;
