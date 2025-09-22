91a39f915fe2ed1f4f2d787379f52a0d4fd0e173
Aviral Kaushal <guest3@Avirals-MacBook-Pro.local>
2025-09-22 19:40:12 +0530

    implemented MessagePage but not able to get stream text response in completion variable of useCompletion

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -4,13 +4,17 @@ import { streamText, InvalidPromptError, APICallError } from "ai";
 export const runtime = "edge";
 
 export async function POST(req: Request) {
+  console.log("This API IS CALLED!!");
   try {
     const prompt =
       "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?||What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
     const result = streamText({
-      model: google("gemini-1.5-flash"),
-      messages: [{ role: "user", content: prompt }],
+      model: google("gemini-2.5-flash"),
+      prompt,
+      onChunk: ({ chunk }) => {
+        console.log("Chunk: ", chunk);
+      },
     });
 
     return result.toTextStreamResponse();
